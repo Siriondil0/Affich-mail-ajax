15870c735ce56ab694e718af973cb115a0bfc3ca
guillaume <elros.du.13@gmail.com>
2018-11-20 23:03:00 +0100

    good dropdown

--- app/assets/javascripts/dropdown_hover.js
+++ app/assets/javascripts/dropdown_hover.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-  $('.nav-dropdown-content').hover(function() {
-    $(this).find('.nav-dropdown-content').stop(true, true).delay(200).fadeIn(500);
-    }, function() {
-    $(this).find('.nav-dropdown-content').stop(true, true).delay(200).fadeOut(500);
-  });
   
+
+  $('.nav-dropdown').hover(
+    function () {
+      $('.nav-dropdown-content', this).stop(true, true).delay(200).fadeIn(500);
+    },
+    function () {
+      $('.nav-dropdown-content', this).stop(true, true).delay(200).fadeOut(500);
+    }
+  );
+
+
 });
